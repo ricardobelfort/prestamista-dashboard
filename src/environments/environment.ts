8cb573a09bfde99e0e260325eabd8f95801ff61c
Ricardo Belfort <rbelfort2004@gmail.com>
2025-10-25 21:36:36 -0400

    fix: ajustando env locais

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  supabaseUrl: '__SUPABASE_URL__',
-  supabaseKey: '__SUPABASE_KEY__',
+  supabaseUrl: 'https://frwawmcvrpdhsuljrvlw.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZyd2F3bWN2cnBkaHN1bGpydmx3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODI1ODQsImV4cCI6MjA3NjY1ODU4NH0.95Om2RvBjsHASPF-wLhjN32ntbMRKK9TwypLgVS23p4',
   features: {
     enableAutoLogin: false,
   }
